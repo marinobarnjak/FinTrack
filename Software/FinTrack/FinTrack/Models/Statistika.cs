08fad60c422fb82aa0be7228432c4ac846e48c01
mbarnjak23 <mbarnjak2@student.foi.hr>
2025-06-20 23:41:22 +0200

    Initial commit: Migrated project to personal repository

---
+++ Software/FinTrack/FinTrack/Models/Statistika.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTrack.Models
+{
+    public class Statistika
+    {
+        public string KategorijaNaziv { get; set; }
+        public decimal UkupanIznos { get; set; }
+        public float Postotak { get; set; }
+    }
+}
